5af56d6f04c4065cbf934c84c731d5a1b372487d
GengGode <GengGode@outlook.com>
2022-11-30 20:43:52 +0800

    Chore 添加Cpp实现

--- g2BeiDou.Capture.Test/Capture.Test.cpp
+++ g2BeiDou.Capture.Test/Capture.Test.cpp
@@ -6,46 +6,59 @@
 #define CATCH_CONFIG_MAIN
 #include "catch2.hpp"
 
-TEST_CASE("Capture.Test CreateCapture WindowHandle", "[CreateCapture WindowHandle]") {
-    g2::Capture *capture = g2::CreateCapture(g2::CaptureType::WindowHandle);
-    REQUIRE(capture != nullptr);
-    g2::CaptureOptions options;
-    options.window_handle = (long long int)GetDesktopWindow();
-    g2::SetWindowHandle(capture, options);
-    //REQUIRE(g2::Initialize(capture));
+TEST_CASE("Capture.Test C_API CreateCapture", "[CreateCapture]") {
+    {
+        g2::Capture *capture = g2::CreateCapture(g2::CaptureType::WindowHandle);
+        REQUIRE(capture != nullptr);
+        g2::CaptureOptions options;
+        options.window_handle = (long long int) GetDesktopWindow();
+        g2::SetWindowHandle(capture, options);
+        
+        g2::DestroyCapture(capture);
+        REQUIRE(capture == nullptr);
+    }
     
-    g2::DestroyCapture(capture);
-    REQUIRE(capture == nullptr);
+    {
+        g2::Capture *capture = g2::CreateCapture(g2::CaptureType::DirectShow);
+        REQUIRE(capture != nullptr);
+    
+        g2::DestroyCapture(capture);
+        REQUIRE(capture == nullptr);
+    }
+    
+    {
+        g2::Capture *capture = g2::CreateCapture(g2::CaptureType::Video);
+        REQUIRE(capture != nullptr);
+    
+        g2::DestroyCapture(capture);
+        REQUIRE(capture == nullptr);
+    }
+    
+    {
+        g2::Capture *capture = g2::CreateCapture(g2::CaptureType::Video2);
+        REQUIRE(capture != nullptr);
+    
+        g2::DestroyCapture(capture);
+        REQUIRE(capture == nullptr);
+    }
+    
+    {
+        g2::Capture *capture = g2::CreateCapture(g2::CaptureType::Video3);
+        REQUIRE(capture != nullptr);
+    
+        g2::DestroyCapture(capture);
+        REQUIRE(capture == nullptr);
+    }
 }
 
-TEST_CASE("Capture.Test CreateCapture DirectShow", "[CreateCapture DirectShow]") {
-    g2::Capture *capture = g2::CreateCapture(g2::CaptureType::DirectShow);
-    REQUIRE(capture != nullptr);
-    
-    g2::DestroyCapture(capture);
-    REQUIRE(capture == nullptr);
-}
-
-TEST_CASE("Capture.Test CreateCapture Video", "[CreateCapture Video]") {
-    g2::Capture *capture = g2::CreateCapture(g2::CaptureType::Video);
-    REQUIRE(capture != nullptr);
-    
-    g2::DestroyCapture(capture);
-    REQUIRE(capture == nullptr);
-}
-
-TEST_CASE("Capture.Test CreateCapture Video2", "[CreateCapture Video2]") {
-    g2::Capture *capture = g2::CreateCapture(g2::CaptureType::Video2);
-    REQUIRE(capture != nullptr);
-    
-    g2::DestroyCapture(capture);
-    REQUIRE(capture == nullptr);
-}
-
-TEST_CASE("Capture.Test CreateCapture Video3", "[CreateCapture Video3]") {
-    g2::Capture *capture = g2::CreateCapture(g2::CaptureType::Video3);
-    REQUIRE(capture != nullptr);
-    
-    g2::DestroyCapture(capture);
-    REQUIRE(capture == nullptr);
+TEST_CASE("Capture.Test CPP_API CaptureManager ", "[CaptureManager]") {
+    std::vector<g2::CaptureType> types = {g2::CaptureType::WindowHandle, g2::CaptureType::DirectShow, g2::CaptureType::Video, g2::CaptureType::Video2, g2::CaptureType::Video3};
+    for (auto type : types) {
+        g2::CaptureManager manager;
+        REQUIRE(true);
+        //REQUIRE(manager.create(type));
+        //REQUIRE(manager.initialize());
+        //REQUIRE(manager.release());
+        //REQUIRE(manager.destroy());
+    }
 }
--- g2BeiDou.Capture/include/g2BeiDou.Capture.hpp
+++ g2BeiDou.Capture/include/g2BeiDou.Capture.hpp
@@ -22,12 +22,16 @@
 #define DEF_Enable_VideoCapture3
 #endif
 
-
-// This class is exported from the g2BeiDou.Capture.dll
+#ifndef __cplusplus
 namespace cv
 {
     class Mat;
 }
+#else
+#include <opencv2/core.hpp>
+#endif
+
+// This class is exported from the g2BeiDou.Capture.dll
 namespace g2
 {
     
@@ -44,41 +48,59 @@ namespace g2
     {
     public:
         CaptureOptions() = default;
-        
         ~CaptureOptions() = default;
-    
     public:
         long long int window_handle = 0;
         long long int direct_handle = 0;
         char *video_path = nullptr;
     };
-    
     class Capture;
-    
-    G2API Capture *CreateCapture(CaptureType mode = CaptureType::WindowHandle);
-    
-    G2API void DestroyCapture(Capture *&capture);
-    
-    G2API void SetWindowHandle(Capture *capture, CaptureOptions &options);
-    
-    G2API void SetDirectHandle(Capture *capture, CaptureOptions &options);
-    
-    G2API void SetVideoPath(Capture *capture, CaptureOptions &options);
-    
-    G2API void UseWindowHandleCapture(Capture *capture);
-    
-    G2API void UseDirectShowCapture(Capture *capture);
-    
-    G2API void UseVideoCapture(Capture *capture);
-    
-    G2API void UseVideoCapture2(Capture *capture);
-    
-    G2API void UseVideoCapture3(Capture *capture);
-    
-    G2API cv::Mat *CaptureImage(Capture *capture);
-    
-    G2API void ReleaseImage(cv::Mat *&image);
 
+    extern "C" {
+    
+        G2API Capture *CreateCapture(CaptureType mode = CaptureType::WindowHandle);
+        
+        G2API void DestroyCapture(Capture *&capture);
+        
+        G2API void SetWindowHandle(Capture *capture,const CaptureOptions &options);
+        
+        G2API void SetDirectHandle(Capture *capture,const CaptureOptions &options);
+        
+        G2API void SetVideoPath(Capture *capture,const CaptureOptions &options);
+        
+        G2API void UseWindowHandleCapture(Capture *capture);
+        
+        G2API void UseDirectShowCapture(Capture *capture);
+        
+        G2API void UseVideoCapture(Capture *capture);
+        
+        G2API void UseVideoCapture2(Capture *capture);
+        
+        G2API void UseVideoCapture3(Capture *capture);
+        
+        G2API cv::Mat *CaptureImage(Capture *capture);
+        
+        G2API void ReleaseImage(cv::Mat *&image);
+    };
+
+#ifdef __cplusplus
+    class G2API CaptureManager
+    {
+        Capture* capture = nullptr;
+        CaptureType mode = CaptureType::WindowHandle;
+        CaptureOptions options;
+    public:
+        explicit CaptureManager(CaptureType mode = CaptureType::WindowHandle);
+        CaptureManager(CaptureType mode,const CaptureOptions &options);
+        
+        ~CaptureManager();
+        
+        void reinitialize(CaptureType mode);
+        void set(const CaptureOptions &options);
+        
+        cv::Mat get();
+    };
+#endif
 }
 //#define DEF_Enable_WindowHandleCapture
 //#define DEF_Enable_DirectShowCapture
